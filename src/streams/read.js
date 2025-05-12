1ff3368544bc708ccdd3ad3c205cb2a220f5100d
dassshka999 <159941766+dassshka999@users.noreply.github.com>
2025-05-13 02:54:49 +0300

    feat:add completed streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,20 @@
+import { createReadStream } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const read = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+    const filePath = path.join(__dirname, 'files', 'fileToRead.txt');
+
+    const stream = createReadStream(filePath, { encoding: 'utf-8' });
+
+    stream.pipe(process.stdout);
+
+    stream.on('error', () => {
+        console.error('FS operation failed');
+    });
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,20 @@
+import { createWriteStream } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const write = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+    const filePath = path.join(__dirname, 'files', 'fileToWrite.txt');
+
+    const writableStream = createWriteStream(filePath, { encoding: 'utf-8' });
+
+    process.stdin.pipe(writableStream);
+
+    writableStream.on('error', () => {
+        console.error('FS operation failed');
+    });
 };
 
 await write();
