614e6c5177c7e9300ce21b7a23f4a52c02d9022f
dassshka999 <159941766+dassshka999@users.noreply.github.com>
2025-05-13 00:50:30 +0300

    feat:add completed cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,15 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2);
+
+    for (let i = 0; i < args.length; i += 2) {
+        const key = args[i];
+        const value = args[i + 1];
+
+        if (key.startsWith('--') && value !== undefined) {
+            const propName = key.slice(2);
+            console.log(`${propName} is ${value}`);
+        }
+    }
 };
 
 parseArgs();
