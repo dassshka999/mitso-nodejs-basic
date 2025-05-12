c98b3344c2c12bda49c50a6d840012bdebf33e76
dassshka999 <159941766+dassshka999@users.noreply.github.com>
2025-05-13 02:37:45 +0300

    feat:add completed hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,21 @@
+import { createHash } from 'crypto';
+import { readFile } from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+    const filePath = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
+    try {
+        const fileBuffer = await readFile(filePath);
+        const hash = createHash('sha256').update(fileBuffer).digest('hex');
+        console.log(hash);
+    } catch (error) {
+        console.error('FS operation failed');
+    }
 };
 
 await calculateHash();
